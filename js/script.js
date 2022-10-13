bc14d985242b7ca72c1aa1b877bc4b69778109bf
Antônio Jefferson <at.jefferson.nascimento@gmail.com>
2022-10-12 23:48:20 -0300

    alguns buns resolvidos, graças a Deus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//PEGANDO OS ELEMENTOS 
 let card1 = document.getElementById('card1');
 let card2 = document.getElementById('card2');
 let card3 = document.getElementById('card3');
@@ -14,11 +15,10 @@ let card13 = document.getElementById('card13');
 let card14 = document.getElementById('card14');
 let card15 = document.getElementById('card15');
 let icon = document.getElementById('icon');
+let botao = document.getElementById('botao');
 
 
-
-
-
+//CRIANDO VARIAVEIS 
 let prato = "";
 let valorPrato = 0;
 let cocoquinha= "";
@@ -31,7 +31,14 @@ let cont2 = 0;
 let cont3 = 0;
 let valor = 0;
 
-//SESSÃO ONE!!!!!! ===================== eu SEI QUE NÃO É A MELHOR FORMA!
+
+let nome = prompt('QUAL SEU NOME: ');
+let endereco = prompt('INFORME SEU ENDEREÇO: ');
+
+
+
+
+//SESSÃO ONE!!!!!! ===================== EU SEI QUE NÃO É A MELHOR FORMA!
 function selecionar1(){
     card1.classList.add('corbordCard');
     card2.classList.remove('corbordCard');
@@ -39,8 +46,8 @@ function selecionar1(){
     card4.classList.remove('corbordCard');
     card4.classList.remove('corbordCard');
     icon.classList.add('icon_visivel')
-    prato = 'comida1';
-    valorPrato = 10.50;
+    prato = 'Frango Yin Yang';
+    valorPrato = 14.90;
     cont1 = 1;
 }
 function selecionar2(){
@@ -49,8 +56,8 @@ function selecionar2(){
   card3.classList.remove('corbordCard');
   card4.classList.remove('corbordCard');
   card4.classList.remove('corbordCard');
-  prato = 'comida2';
-  valorPrato = 12.50;
+  prato = 'Cassoulet';
+  valorPrato = 20,00;
   cont1 = 1;
 }
 function selecionar3(){
@@ -59,8 +66,8 @@ function selecionar3(){
   card2.classList.remove('corbordCard');
   card4.classList.remove('corbordCard');
   card5.classList.remove('corbordCard');
-  prato = 'coomida3';
-  valorPrato = 15.00;
+  prato = 'Strogonoff';
+  valorPrato = 15.99;
   cont1 = 1;
 }
 function selecionar4(){
@@ -69,8 +76,8 @@ function selecionar4(){
   card5.classList.remove('corbordCard');
   card1.classList.remove('corbordCard');
   card2.classList.remove('corbordCard');
-  prato = 'coomida4';
-  valorPrato = 13.50;
+  prato = 'Carne suína assada';
+  valorPrato = 20.00;
   cont1 = 1;
 }
 function selecionar5(){
@@ -79,8 +86,8 @@ function selecionar5(){
   card1.classList.remove('corbordCard');
   card2.classList.remove('corbordCard');
   card3.classList.remove('corbordCard');
-  prato = 'coomida5';
-  valorPrato = 8.50;
+  prato = 'Fettuccine';
+  valorPrato = 30.00;
   cont1 = 1;
 }
 
@@ -92,8 +99,8 @@ function selecionar6(){
   card8.classList.remove('corbordCard');
   card9.classList.remove('corbordCard');
   card10.classList.remove('corbordCard');
-  cocoquinha = 'coco1';
-  valorCoca = 5.50;
+  cocoquinha = 'Cocacola';
+  valorCoca = 3.00;
   cont2 = 1;
 }
 function selecionar7(){
@@ -102,8 +109,8 @@ function selecionar7(){
   card8.classList.remove('corbordCard');
   card9.classList.remove('corbordCard');
   card10.classList.remove('corbordCard');
-  cocoquinha = 'coco2';
-  valorCoca = 3.50;
+  cocoquinha = 'Guarana Jesus';
+  valorCoca = 3.00;
   cont2 = 1;
 }
 function selecionar8(){
@@ -112,8 +119,8 @@ function selecionar8(){
   card7.classList.remove('corbordCard');
   card9.classList.remove('corbordCard');
   card10.classList.remove('corbordCard');
-  cocoquinha = 'coco3';
-  valorCoca = 1.00;
+  cocoquinha = 'Suco natural';
+  valorCoca = 5.00;
   cont2 = 1;
 }
 function selecionar9(){
@@ -122,8 +129,8 @@ function selecionar9(){
   card7.classList.remove('corbordCard');
   card8.classList.remove('corbordCard');
   card10.classList.remove('corbordCard');
-  cocoquinha = 'coco4';
-  valorCoca = 2.00;
+  cocoquinha = 'Fanta';
+  valorCoca = 3.00;
   cont2 = 1;
 }
 function selecionar10(){
@@ -132,8 +139,8 @@ function selecionar10(){
   card7.classList.remove('corbordCard');
   card8.classList.remove('corbordCard');
   card9.classList.remove('corbordCard');
-  cocoquinha = 'coco5';
-  valorCoca = 2.50;
+  cocoquinha = 'Haineken';
+  valorCoca = 30.00;
   cont2 = 1;
 }
 
@@ -144,8 +151,8 @@ function selecionar11(){
   card13.classList.remove('corbordCard');
   card14.classList.remove('corbordCard');
   card15.classList.remove('corbordCard');
-  sobremesa = 'pudin1';
-  valorSobremesa = 12.50;
+  sobremesa = 'Pudim';
+  valorSobremesa = 12.00;
   cont3 = 1;
   }
 function selecionar12(){
@@ -154,8 +161,8 @@ function selecionar12(){
   card13.classList.remove('corbordCard');
   card14.classList.remove('corbordCard');
   card15.classList.remove('corbordCard');
-  sobremesa = 'pudin1';
-  valorSobremesa = 10.00;
+  sobremesa = 'Coxinha';
+  valorSobremesa = 2.50;
   cont3 = 1;
 }
 function selecionar13(){
@@ -164,8 +171,8 @@ function selecionar13(){
   card11.classList.remove('corbordCard');
   card14.classList.remove('corbordCard');
   card15.classList.remove('corbordCard');
-  sobremesa = 'pudin1';
-  valorSobremesa = 15.00;
+  sobremesa = 'Pão de queijo';
+  valorSobremesa = 1.50;
   cont3 =  1;
   }
 function selecionar14(){
@@ -174,8 +181,8 @@ function selecionar14(){
   card13.classList.remove('corbordCard');
   card11.classList.remove('corbordCard');
   card15.classList.remove('corbordCard');
-  sobremesa = 'pudin1';
-  valorSobremesa = 4.50;
+  sobremesa = 'Soverte na taça';
+  valorSobremesa = 9.90;
   cont3 =  1;
   }
 function selecionar15(){
@@ -184,34 +191,30 @@ function selecionar15(){
   card13.classList.remove('corbordCard');
   card14.classList.remove('corbordCard');
   card11.classList.remove('corbordCard');
-  sobremesa = 'pudin1';
-  valorSobremesa = 2.50;
+  sobremesa = 'Pastel de frango';
+  valorSobremesa = 4.80;
   cont3 =  1;
   }
-
-// Fazer o pedido !!!
-total = valorCoca +  valorPrato + valorSobremesa;
-valor = cont1 + cont2 + cont3;
-
-console.log(valor)
-console.log(sobremesa)
-console.log(prato)
-console.log(cocoquinha)
-console.log(valorCoca)
-console.log(valorSobremesa)
-console.log(valorPrato)
-
-let msg = encodeURIComponent
-(`
-Olá, gostaria de fazer o pedido:
- - Prato:${prato} valor:R$${valorPrato}
- - Bebida:${cocoquinha} valor:R$${valorCoca}
- - Sobremesa:${sobremesa} valor:R$${valorSobremesa}
- Total: R$${total}`)
-
-function fazerPeido(){
- if(valor == 0){
-  window.open('https://wa.me/5598984358019?text=' + msg);
- }
+ 
+//fUNÇÃO PARA FAZER O PEDIDO
+function fazerPedido(){
+  total = valorCoca +  valorPrato + valorSobremesa;
+  valor = cont1 + cont2 + cont3;
+  valor.toFixed(2)
+  let msg = encodeURIComponent
+  (`
+  *Olá, gostaria de fazer o pedido:*
+   *- Prato:* ${prato}                ${valorPrato}
+   *- Bebida:* ${cocoquinha}          ${valorCoca}
+   *- Sobremesa:* ${sobremesa}        ${valorSobremesa}
+     *Total:* *R$${total}*
+   --------------------------------------------------
+   *Nome:* ${nome};
+   *Endereço:* ${endereco}
+   `)
+   
+  if(valor == 3){
+    window.open('https://wa.me/5598984358019?text=' + msg);
+  }
   
 }
