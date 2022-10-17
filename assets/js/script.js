13a9223eb9e5b868441780567a248c379a156b0d
Antônio Jefferson <at.jefferson.nascimento@gmail.com>
2022-10-16 21:02:50 -0300

    bônus feito, só que tem um bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ function selecionarComida(comidaSelecionada){
 
   //Pegando o nome da Comida
   const nomeComida = document.querySelector('.bordComida > h3')
-  comida =  nomeComida.innerText; 
+  comida =  nomeComida.innerHTML; 
 
   //pegar valor da comida
   const valorPrato = document.querySelector('.bordComida > h4 > strong')
@@ -50,7 +50,7 @@ function selecionarCoca(cocaSelecionada){
 
   //Pegando o nome da bebida 
   const nomeCoca = document.querySelector('.bordCoca > h3')
-  coca =  nomeCoca.innerText;
+  coca =  nomeCoca.innerHTML;
 
   //Pegar valor da bebida
   const valorCoca = document.querySelector('.bordCoca > h4 > strong')
@@ -77,7 +77,7 @@ function selecionarSobremesa(sobremesaSelecionada){
 
   //Pegando o nome da Sobremesa 
   const nomeSobremesa = document.querySelector('.bordSobremesa > h3')
-  sobremesa =  nomeSobremesa.innerText;
+  sobremesa =  nomeSobremesa.innerHTML;
 
   //Pegar valor da Sobremesa
   const valorSobre = document.querySelector('.bordSobremesa > h4 > strong')
@@ -115,11 +115,40 @@ function ativarBTN() {
     }
   }
 /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+function telaConfirmacao(){
+  if(comida !== undefined && coca !== undefined && sobremesa !== undefined){
+        const tela = document.querySelector('.teladeConfirmacao');
+        tela.classList.remove('teladeConfirmacao');
+      
+        const p_comida = document.querySelector('.p_comida');
+        const p_valorComida = document.querySelector('.p_valorComida');
+        p_comida.innerHTML = comida;
+        p_valorComida.innerHTML = valorComida;
 
+        const p_bebida = document.querySelector('.p_bebida');
+        const p_valorBebida = document.querySelector('.precoBebida');
+        p_bebida.innerHTML = coca;
+        p_valorBebida.innerHTML = valorBebida;
+
+        const nomeSobremesa = document.querySelector('.nomeSobremesa');
+        const precoSobremesa = document.querySelector('.precoSobremesa');
+        nomeSobremesa.innerHTML = sobremesa;
+        precoSobremesa.innerHTML = valorSobremesa;
+
+        const valorTotal = document.querySelector('.precoTotal');
+        //Tirando a "," e colocando o "." usando o replace()
+        const nu1 = valorComida.replace(',','.');
+        const nu2 = valorBebida.replace(',','.');
+        const nu3 = valorSobremesa.replace(',', '.');
+        //Convertendo para número e fazendo a soma
+        total = Number(nu1) + Number(nu2) + Number(nu3);
+        const precoFinal = total.toFixed(2).replace('.', ',')
+        valorTotal.innerHTML = precoFinal;
+  }
+}
 //Funcção para enviar o pedido!!! ==================================================
 function fazerPedido(){
-  if(comida !== undefined && coca !== undefined && sobremesa !== undefined){
-     //Perguntando o endereço e o nome do usuario
+  //Perguntando o endereço e o nome do usuario
   const nome = prompt("QUAL SEU NOME?:");
   const endereco = prompt("ENDEREÇO DE ENTREGA: ")
 
@@ -129,19 +158,23 @@ function fazerPedido(){
   const nu3 = valorSobremesa.replace(',', '.');
   //Convertendo para número e fazendo a soma
   total = Number(nu1) + Number(nu2) + Number(nu3);
-
+  
   let msg = encodeURIComponent
   (`
   Olá, gostaria de fazer o pedido:*
   - Prato: ${comida}               
   - Bebida: ${coca}        
   - Sobremesa: ${sobremesa}       
-  Total: R$${total}
+  Total: R$${total.toFixed(2)}
 
   Nome: ${nome};
   Endereço: ${endereco}
    `)
   window.open('https://wa.me/5598985856218?text=' + msg);
-  }
- alert('Você precisa escolher as 3 opções!')
+}
+
+function cancelar(){
+  const semIdeiaDeNomeMais = document.querySelector('.idetificador');
+  semIdeiaDeNomeMais.classList.remove('visivel');
+  semIdeiaDeNomeMais.classList.add('teladeConfirmacao')
 }
